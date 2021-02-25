bf2ac217c16759b4c3416f32441e11bcc44643a7
Macintosh <drow724@naver.com>
2021-02-25 12:54:51 +0900

    Getters와Setters 그리고 이것을 써야하는 이유

---
+++ src/part3/ex3/Getters와Setters/Exam.java
@@ -0,0 +1,31 @@
+package part3.ex3.Getters와Setters;
+
+public class Exam {
+	int kor1;
+	int eng;
+	int math;
+	public int getKor() {
+		
+		return kor1;
+	}
+	public int getEng() {
+
+		return eng;
+	}
+	public int getMath() {
+
+		return math;
+	}
+	public void setKor(int kor) {
+		this.kor1 = kor;
+		
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+		
+	}
+	public void setMath(int math) {
+		this.math = math;
+		
+	}
+}
