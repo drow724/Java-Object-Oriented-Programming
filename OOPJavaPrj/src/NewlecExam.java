14c4c4fb4fa98669f1a5dffc1eebafd0173ac68f
Macintosh <drow724@naver.com>
2021-02-26 23:03:16 +0900

    Exam을 IS A  상속

---
+++ OOPJavaPrj/src/NewlecExam.java
@@ -0,0 +1,16 @@
+import part3.ex3.UI코드분리하기.Exam;
+
+public class NewlecExam extends Exam{
+//	private Exam exam;
+	
+	private int com;
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+	
+}
--- OOPJavaPrj/src/Program.java
+++ OOPJavaPrj/src/Program.java
@@ -2,15 +2,12 @@ import part3.ex3.UI코드분리하기.Exam;
 
 public class Program {
 
-	//1. 컴파일 -> Exam.class
-	//2. 압축 -> Exam.zip
-	//3. jar -> Exam.jar
-	
-	//>javac -cp "d:\java\examlib.jar" Program.java
-	
 	public static void main(String[] args) {
-		Exam exam= new Exam(1, 1, 1);
-		
+		NewlecExam exam= new NewlecExam();
+		exam.setKor(10);
+		exam.setEng(10);
+		exam.setMath(10);
+		exam.setCom(10);//total값에 포함되지 않음
 		System.out.println(exam.total());
 	}
 }
