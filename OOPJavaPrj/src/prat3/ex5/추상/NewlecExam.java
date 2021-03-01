8eaecb55ee9a7c7313fd9f2789627066d926f43d
Macintosh <drow724@naver.com>
2021-03-01 18:43:49 +0900

    추상 클래스 만들기 추상화

---
+++ OOPJavaPrj/src/prat3/ex5/추상/NewlecExam.java
@@ -0,0 +1,7 @@
+package prat3.ex5.추상;
+
+import part3.ex5.추상.Exam;
+
+public class NewlecExam extends Exam {
+
+}
---
+++ OOPJavaPrj/src/prat3/ex5/추상/Program.java
@@ -0,0 +1,12 @@
+package prat3.ex5.추상;
+
+import part3.ex5.추상.Exam;
+
+public class Program {
+
+	public static void main(String[] args) {
+		NewlecExam exam = new NewlecExam();
+
+	}
+
+}
