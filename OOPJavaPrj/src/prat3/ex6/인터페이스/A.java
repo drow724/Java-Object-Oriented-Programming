6f62d929feb7dd55b954bd528c574d75fb0b7a4e
Macintosh <drow724@naver.com>
2021-03-02 06:57:40 +0900

    객체 단위로 분리/결합을 위한 인터페이스 구현하기

---
+++ OOPJavaPrj/src/prat3/ex6/인터페이스/A.java
@@ -0,0 +1,15 @@
+package prat3.ex6.인터페이스;
+
+public class A {
+	private B b;
+	
+	public A() {
+		b= new B();
+	}
+	
+	public void print() {
+		int total = b.total();
+		
+		System.out.printf("total is %d\n", total);
+	}
+}
---
+++ OOPJavaPrj/src/prat3/ex6/인터페이스/B.java
@@ -0,0 +1,10 @@
+package prat3.ex6.인터페이스;
+
+public class B {
+
+	public int total() {
+		
+		return 30;
+	}
+	
+}
