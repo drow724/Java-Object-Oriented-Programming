e1236a349506170fbb640eab447b0ddc6b586b22
Macintosh <drow724@naver.com>
2021-03-03 20:24:49 +0900

    일부 기능을 분리하는 인터페이스

--- GamePrj/src/Program.java
+++ GamePrj/src/Program.java
@@ -1,9 +1,12 @@
 import java.awt.Frame;
+import java.awt.event.WindowListener;
 
 public class Program {
 
 	public static void main(String[] args) {
 		GameFrame frame = new GameFrame();
+		WindowListener listener = new GameWindowLisnter();
+		frame.addWindowListener(listener);
 		frame.setVisible(true);
 	}
 
