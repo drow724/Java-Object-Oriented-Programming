ff9ad2fe0a0befd73fbf175eacbd00a13102a261
Macintosh <drow724@naver.com>
2021-03-03 20:31:28 +0900

    인터페이스를 구현하는 위치는?

--- GamePrj/src/Program.java
+++ GamePrj/src/Program.java
@@ -5,9 +5,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		GameFrame frame = new GameFrame();
-		WindowListener listener = new GameWindowLisnter();
-		frame.addWindowListener(listener);
-		frame.setVisible(true);
+		//WindowListener listener = new GameWindowLisnter();
+		//frame.addWindowListener(listener);
+		//frame.setVisible(true);
 	}
 
 }
